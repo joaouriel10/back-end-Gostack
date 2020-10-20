75d3dc23b4812f6df86ffd4cc93d26a21cd50189
Joaouriel10 <joaouriel2016@outlook.com>
2020-10-19 22:03:18 -0300

    GoStack

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,17 +5,13 @@ import AutenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const autenticateUser = new AutenticateUserService();
+  const autenticateUser = new AutenticateUserService();
 
-    const { user, token } = await autenticateUser.execute({ email, password });
+  const { user, token } = await autenticateUser.execute({ email, password });
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -16,7 +18,7 @@ class CreateUserService {
     const checkUserExists = await userRepository.findOne({ where: { email } });
 
     if (checkUserExists) {
-      throw new Error('Email addres alredy used');
+      throw new AppError('Email addres alredy used');
     }
 
     const hashedPassword = await hash(password, 8);
